90a680dd37adaa4d02980a5db76ddb27a50a32e8
Roman Kaluger <r.kaluger@sftpro.ru>
2023-09-12 22:17:02 +0300

    new api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,21 @@
 import { Socket } from 'socket.io';
-import { ApiHandlers } from './types';
+import { ApiHandlers, SocketResponse } from './types';
 
 export class Api<T extends string> {
   constructor(private handlers: ApiHandlers<T>) {}
 
   async handle(action: T, socket: Socket, payload: { requestId?: string }) {
     try {
-      await this.handlers[action](socket, payload);
+      const answer = await this.handlers[action](payload, socket);
+
+      if (answer && payload.requestId) {
+        const successResponse: SocketResponse<typeof answer> = {
+          requestId: payload.requestId,
+          payload: answer,
+        };
+
+        socket.emit(action, successResponse);
+      }
     } catch ({ message }) {
       payload.requestId &&
         socket.emit(action, { requestId: payload.requestId, error: message });
--- src/api/types.ts
+++ src/api/types.ts
@@ -1,7 +1,7 @@
 import { Socket } from 'socket.io';
 
 export type Request<T> = T & { requestId?: string };
-export type SocketResponse<T = 'success' | 'error'> = {
+export type SocketResponse<T> = {
   requestId?: string;
   error?: string;
   payload: T;
@@ -18,6 +18,6 @@ export enum SocketErrors {
 export type ApiHandlers<ACTIONS extends string> = Record<ACTIONS, ApiHandler>;
 
 export type ApiHandler = (
-  socket: Socket,
   payload: unknown,
-) => void | Promise<void>;
+  socket?: Socket,
+) => unknown | Promise<unknown>;
